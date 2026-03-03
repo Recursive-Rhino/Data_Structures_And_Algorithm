516280e0c26640ce32494b8ec2aedee7f94c46e4
Recursive-Rhino <smruti1234qwerty@gmail.com>
2026-03-03 23:59:05 +0530

    Bit Manipulation

--- src/Bit_Manipulation/SetOrNot/UsingRightShift.java
+++ src/Bit_Manipulation/SetOrNot/UsingRightShift.java
@@ -1,19 +1,19 @@
-package Bit_Manipulation;
+package Bit_Manipulation.SetOrNot;
 
 import java.util.Scanner;
 
-public class IthBitSetOrNot {
+public class UsingRightShift {
     static void main() {
         System.out.println("Enter the number : ");
         Scanner input=new Scanner(System.in);
         int num=input.nextInt();
         System.out.println("Enter the it pos to check : ");
         int i=input.nextInt();
-        int ans=num & (1<<i);
-        if (ans>0){
+        int ans=(num>>i)&1;
+        if(ans>0) {
             System.out.println("Set");
-        } else{
-            System.out.println("Not set");
+        } else {
+            System.out.println("Not Set");
         }
 
     }
---
+++ src/Bit_Manipulation/SetTheIthBit/ByOrGate.java
@@ -0,0 +1,15 @@
+package Bit_Manipulation.SetTheIthBit;
+
+import java.util.Scanner;
+
+public class ByOrGate {
+    static void main() {
+        System.out.println("Enter the number : ");
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        System.out.println("Enter the bitPos : ");
+        int i=input.nextInt();
+        int ans=num | (1<<i);
+        System.out.println(ans);
+    }
+}
